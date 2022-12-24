cc193877288167c4bd6c19550884252553d60e77
vladimir <vovazakharov2@gmail.com>
2022-12-24 16:12:13 +0300

    added UserStatistic model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from tests.models import Test
 
 
 class User(AbstractUser):
     pass
+
+
+class UserStatistic(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.SET_NULL)
+    result = models.IntegerField(default=0)
